42d78632031e3c0cea8fd5dfa8989b94920b9e29
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-04-21 15:32:53 +0530

    done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -10,7 +10,7 @@ export default function Body({ data }) {
 
   function filterData(searchText, restraunt) {
     return restraunt.filter((data) => {
-      return data.name.includes(searchText);
+      return data.name.toLowerCase() === searchText.toLowerCase();
     });
   }
 
